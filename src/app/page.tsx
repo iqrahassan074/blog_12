0127ea3c23c3c33c6b1a18a98b1b97ccf1834b37
iqrahassan074 <iqrasher142@gmail.com>
2025-06-03 21:57:32 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,21 @@
+// src/app/page.tsx
 import React from "react";
-import Mega from '../components/Mega';
-import Feature from '../components/Feature';
-import Dir from "@/components/Dir";
-import Navbar from '../components/Navbar';
-import Footer from "@/components/Footer";
+import Mega from '@/components/Mega';
+import Feature from '@/components/Feature';
+import Dir from '@/components/Dir';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 
 export default function Home() {
   return (
-    <div className="bg-gradient-to-r from-green-400 to-green-600 via-green-800">
-      <Navbar/>
-      <Mega/>
-      <Feature/>
-      <Dir/>
-      <Footer/>
+    <div className="bg-gradient-to-r from-green-400 to-green-600 via-green-800 min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-grow">
+        <Mega />
+        <Feature />
+        <Dir />
+      </main>
+      <Footer />
     </div>
   );
 }
-
